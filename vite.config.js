cd3be2319ec190658ff7a9590a8707d0baa52813
wuyi0020 <wuyi0020@gmail.com>
2023-06-08 07:58:08 +0800

    new base url

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vue from "@vitejs/plugin-vue";
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [vue()],
-    base:"/Arknight_buildingBUFF/",
+    base:"/Arknight_web/",
     server: {
         host: "0.0.0.0",
     },
